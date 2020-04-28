f9427dcfbdd5a704f2248457460b6c2128d59081
klaudia <hernandezklaudia19@gmail.com>
2020-04-28 01:57:07 -0700

    added App

---
+++ todoproject/frontend/src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom";
+
+class App extends Component {
+  render() {
+    return <h1>hello</h1>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
