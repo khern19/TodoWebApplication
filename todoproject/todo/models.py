f06b09423cf734ebe8885cde88c05cd000a6ed06
klaudia <hernandezklaudia19@gmail.com>
2020-04-28 00:46:20 -0700

    created app frontend

--- todoproject/todo/models.py
+++ todoproject/todo/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 
 class Todo(models.Model):
     description = models.CharField(max_length=500)
-    # owner = models.ForeignKey(
-    #     User, related_name="todo", on_delete=models.CASCADE, null=True)
-    # created_at = models.DateTimeField(auto_now_add=True, null=True)
+    owner = models.ForeignKey(
+        User, related_name="todo", on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
