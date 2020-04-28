242fe0711e0a5aef308295bf804403e66e6089a0
klaudia <hernandezklaudia19@gmail.com>
2020-04-28 00:21:38 -0700

    added api

---
+++ todoproject/todo/api.py
@@ -0,0 +1,17 @@
+from todo.models import Todo
+from rest_framework import viewsets, permissions
+from .serializers import TodoSerializer
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+
+    serializer_class = TodoSerializer
+
+    def get_queryset(self):
+        return self.request.user.todo.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
