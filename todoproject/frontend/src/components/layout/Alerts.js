8e444529a3e7dd2232d8e66053d196b532424a28
klaudia <hernandezklaudia19@gmail.com>
2020-04-28 02:39:53 -0700

    alerts when todo items are being added or deleted

---
+++ todoproject/frontend/src/components/layout/Alerts.js
@@ -0,0 +1,31 @@
+import React, { Component, Fragment } from "react";
+import { withAlert } from "react-alert";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+export class Alerts extends Component {
+  static propTypes = {
+    error: PropTypes.object.isRequired,
+    message: PropTypes.object.isRequired,
+  };
+
+  componentDidUpdate(prevProps) {
+    const { error, alert, message } = this.props;
+    if (message !== prevProps.message) {
+      if (message.deleteItem) alert.success(message.deleteItem);
+      if (message.addItem) alert.success(message.addItem);
+      if (message.passwordNotMatch) alert.error(message.passwordNotMatch);
+    }
+  }
+
+  render() {
+    return <Fragment />;
+  }
+}
+
+const mapStateToProps = (state) => ({
+  error: state.errors,
+  message: state.messages,
+});
+
+export default connect(mapStateToProps)(withAlert()(Alerts));
