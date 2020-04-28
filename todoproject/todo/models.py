3ef237f358a98ab1d983461779403c7c1f5f42b8
klaudia <hernandezklaudia19@gmail.com>
2020-04-28 00:18:47 -0700

    filled out model

--- todoproject/todo/models.py
+++ todoproject/todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Todo(models.Model):
+    description = models.CharField(max_length=500)
+    owner = models.ForeignKey(
+        User, related_name="todo", on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
